574dde3d62919200953d9afca12791f3e245e09a
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-07-21 20:07:57 +0300

    Assignment completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { image } from "../data/data";
+import {image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div>
+    <div id="about">
+    <h2 >About Me</h2>
+    <p>I made this</p>
+    <img src= {image} alt = "I made this"/>
+    </div>
+  </div>
+  );
 }
 
 export default About;
